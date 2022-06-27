78506210c04077a248dded5a84bd800e6abf5f11
henrytanglobal <henrytan0831@gmail.com>
2022-06-27 23:59:01 +0200

    Added one more post and changed a few pictures

--- src/community.js
+++ src/community.js
@@ -7,12 +7,13 @@ import s1 from './icon/s1.png';
 import s2 from './icon/s2.png';
 import s3 from './icon/s3.png';
 import s4 from './icon/s4.png';
+import s5 from './icon/s5.jpeg';
 
 import a1 from './icon/a1.png';
 import a2 from './icon/a2.png';
 import a3 from './icon/a3.png';
 import a4 from './icon/a4.png';
-
+import a5 from './icon/a5.jpg';
 
 function Community() {
   return (
@@ -41,7 +42,7 @@ function Community() {
                 <img src={a1} className="Topic-photo-small"/>
               </div>
               <div className="Topic-text2">
-                {"  244568 Views;    10323 Likes;   12 comments"}
+                {"  244568 Views;    10323 Inspired;   12 comments"}
               </div>          
             </div>
         </div>      
@@ -51,13 +52,13 @@ function Community() {
           </div>
             <div className="Topic-text">
               <div className="Topic-text1">
-                {"Jane Donated 1000 USDC to World Food Programme"}
+                {"Jane Donated 10000 USDC to World Food Programme"}
               </div>
               <div className="Topic-photo-small-div">
                 <img src={a2} className="Topic-photo-small"/>
               </div>
               <div className="Topic-text2">
-                {"  244568 Views;    10323 Likes;   12 comments"}
+                {"  244 Views;    23 Inspired;   3 comments"}
               </div>          
             </div>
         </div>      
@@ -74,7 +75,7 @@ function Community() {
                 <img src={a3} className="Topic-photo-small"/>
               </div>
               <div className="Topic-text2">
-                {"  244568 Views;    10323 Likes;   12 comments"}
+                {"  568 Views;    323 Inspired;   5 comments"}
               </div>          
             </div>
         </div>      
@@ -91,10 +92,28 @@ function Community() {
                 <img src={a4} className="Topic-photo-small"/>
               </div>
               <div className="Topic-text2">
-                {"  244568 Views;    10323 Likes;   12 comments"}
+                {"  44568 Views;    1323 Inspired;   3 comments"}
               </div>          
             </div>
         </div>
+	  
+	   <div className="App-topic-cell">
+          <div className="Topic-photo-div">
+            <img src={s5} className="Topic-photo"/>
+          </div>
+            <div className="Topic-text">
+              <div className="Topic-text1">
+                {"Sign the petition to ban single-use plastic bags in supermarket in the State of Washington"}
+              </div>
+              <div className="Topic-photo-small-div">
+                <img src={a5} className="Topic-photo-small"/>
+              </div>
+              <div className="Topic-text2">
+                {"  2445 Views;    1033 Inspired;   1 comments"}
+              </div>          
+            </div>
+        </div>
+	  
     </div>
   );
 }
